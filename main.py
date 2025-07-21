f0d368e9aef856372226f00e2e7e95b934c66b28
hekkort <harryvanderknaap@gmail.com>
2025-07-21 13:40:22 +0200

    updated some logic, still a bit messy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.battle_interface import *
 
 
 def main():
-    moves = [" 1. flamethrower", " 2. sandattack", " 3. mud-slap", " 4. earthquake"]
+    moves = {" 1. flamethrower": 120, " 2. sandattack": 0, " 3. mud-slap": 40, " 4. earthquake": 100}
     print(draw_field(48, moves, "fronk", "harry"))
     
 
