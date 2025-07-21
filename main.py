de010ee6b4f8f6653225aca96cd6dd4ded3d355c
hekkort <harryvanderknaap@gmail.com>
2025-07-21 13:47:12 +0200

    added more stuff

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.battle_interface import *
 
 def main():
     moves = {" 1. flamethrower": 120, " 2. sandattack": 0, " 3. mud-slap": 40, " 4. earthquake": 100}
-    print(draw_field(48, moves, "fronk", "harry"))
+    print(draw_field(48, moves, "jan wouter verbeek", "robby stofbergen"))
     
 
 
