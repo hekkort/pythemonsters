7c94d2ca53bfdc82de5440cc559986604289afd6
hekkort <harryvanderknaap@gmail.com>
2025-07-22 11:07:52 +0200

    hello

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from battle_interface import *
-from pythemon import *
+import pythemon
 
 def main():
     root = "/home/hekkort/workspace/github.com/hekkort/pythemon/monsters"
     bulbasaur_back = root + "/bulbasaur_back.txt"
     bulbasaur_front = root + "/bulbasaur_front.txt"
+
     
-    bulba_lines_back = 
 
 
 if __name__ == "__main__":
